5b070f8ed6ae94e498c1abec69000c0c1f36dfc2
Érico Andrei <erico@simplesconsultoria.com.br>
2011-10-14 11:12:09 -0300

    Find and Replace :-)

--- ZenPacks/community/Varnish/interfaces.py
+++ ZenPacks/community/Varnish/interfaces.py
@@ -8,16 +8,16 @@ class IVarnishDataSourceInfo(IRRDDataSourceInfo):
     timeout = schema.Int(title=_t(u'Timeout (seconds)'))
     cycletime = schema.Int(title=_t(u'Cycle Time (seconds)'))
     hostname = schema.Text(title=_t(u'Host Name'),
-                           group=_t('HTTP Monitor'))
+                           group=_t('Varnish Monitor'))
     port = schema.Int(title=_t(u'Port'),
-                      group=_t('HTTP Monitor'))
+                      group=_t('Varnish Monitor'))
     ipAddress = schema.Text(title=_t(u'Ip Address'),
-                            group=_t('HTTP Monitor'))
+                            group=_t('Varnish Monitor'))
     url = schema.Text(title=_t(u'URL'),
-                      group=_t('HTTP Monitor'))
+                      group=_t('Varnish Monitor'))
     useSsl = schema.Bool(title=_t(u'Use SSL?'),
-                         group=_t('HTTP Monitor'))
+                         group=_t('Varnish Monitor'))
     basicAuthUser = schema.Text(title=_t(u'Basic Auth User'),
-                                group=_t('HTTP Monitor'))
+                                group=_t('Varnish Monitor'))
     basicAuthPass = schema.Password(title=_t(u'Basic Auth Password'),
-                                    group=_t('HTTP Monitor'))
+                                    group=_t('Varnish Monitor'))
